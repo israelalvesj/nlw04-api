37dd33928f3fec7737993462aa73d777bb73072a
israelalvesj <israel.alves05@gmail.com>
2021-02-25 00:17:39 -0300

    Aula 02

---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+}
+
+export {User}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import "reflect-metadata";
 import express from 'express';
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    return res.json({message: "Hello World - NLW04"});
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (req, res) => {
-    return res.json({message: "Dados salvos com sucesso"});
-})
-
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, ()=>console.log("Server is running!"));
