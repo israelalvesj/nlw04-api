4be502c650ae4f925df2695b43dcecf13fbf80f4
Israel Alves de Lima Junior <04473707156@jdemito.local>
2021-03-11 17:57:10 -0300

    Aula 04

---
+++ src/database/migrations/1615474898828-CreateSurveysUsers.ts
@@ -0,0 +1,56 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveysUsers1615474898828 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "surveys_users", 
+            columns:[
+                {
+                    name: "id",
+                    type: "uuid",
+                    isPrimary: true
+                },
+                {
+                    name: "user_id",
+                    type: "uuid",
+                },
+                {
+                    name: "survey_id",
+                    type: "uuid"
+                },
+                {
+                    name: "value",
+                    type: "number",
+                    isNullable: true
+                },
+                {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()"
+                }
+
+        ],
+        foreignKeys: [
+            {
+                name: "FKUser",
+                referencedTableName: "users",
+                referencedColumnNames: ["id"],
+                columnNames: ["user_id"]
+            },
+            {
+                name: "FKSurvey",
+                referencedTableName: "surveys",
+                referencedColumnNames: ["id"],
+                columnNames: ["suervey_id"]
+            }
+        ]
+
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("surveys_users");
+    }
+
+}
